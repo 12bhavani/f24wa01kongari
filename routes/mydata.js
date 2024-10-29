8071cd8c1c2db705d4cc82623cbbe51608ec2b4c
Bhavani Kongari <s575201@nwmissouri.edu>
2024-10-28 19:10:09 -0500

    updates on mydata

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET mydata page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Bhavani Kongari' });
+  res.render('mydata', { title: 'Bhavani Kongari' });
 });
 
 module.exports = router;
